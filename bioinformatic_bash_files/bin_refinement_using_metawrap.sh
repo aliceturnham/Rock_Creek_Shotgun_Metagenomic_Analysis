12c7545cf4049b834c896748b127936aa0ca51fa
Alice Turnham <aturnha1@jhu.edu>
2023-04-21 11:27:23 -0400

    Made changes to set up and run meta-wrap environment on RockFish

--- bioinformatic_bash_files/bin_refinement_using_metawrap.sh
+++ bioinformatic_bash_files/bin_refinement_using_metawrap.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 #SBATCH
 #SBATCH --job-name=binrefinement
-#SBATCH --time=72:00:00
+#SBATCH --time=48:00:00
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=48
-#SBATCH --partition=lrgmem
+#SBATCH --ntasks-per-node=1
+#SBATCH --partition=bigmem
+#SBATCH -A sprehei1_bigmem
+#SBATCH --mail-type=END
+#SBATCH --mail-user=aturnha1@jhu.edu
 
-#export PATH="~/.conda/envs/metawrap-env/bin:$PATH"
+
+
+export PATH=/scratch4/sprehei1/AliceTurnham/lib/metaWRAP/bin/:$PATH:
 #source ~/.bashrc
 
 date
@@ -14,12 +19,18 @@ echo "starting bin consolidation"
 
 ml anaconda
 conda activate metawrap-env
-which python
+#which python
+
+
+curl -L -O https://data.ace.uq.edu.au/public/CheckM_databases/checkm_data_2015_01_16.tar.gz
+tar xzf checkm_data_2015_01_16.tar.gz 
+checkm data setRoot .
+
 
 #make sure mkdir bin_refinement
 OUTDIR=../BIN_REFINEMENT
 O
-mkdir -p $OUTDIR
+#mkdir -p $OUTDIR
 IN=../BINNING
 
 metawrap bin_refinement -o ${OUTDIR} -t 1 -A ${IN}/metabat2_bins/ -B ${IN}/maxbin2_bins/ -C $/${IN}/concoct_bins/ -c 70 -x 5
--- bioinformatic_bash_files/binning.sh
+++ bioinformatic_bash_files/binning.sh
@@ -1,25 +1,48 @@
 #!/bin/sh
 #SBATCH
 #SBATCH --job-name=binning
-#SBATCH --time=72:00:00
+#SBATCH --time=8:00:00
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=48
-#SBATCH --partition=lrgmem
+#SBATCH --ntasks-per-node=24
+#SBATCH --partition=bigmem
+#SBATCH -A sprehei1_bigmem
+#SBATCH --mail-type=END
+#SBATCH --mail-user=aturnha1@jhu.edu
+
+
+
 
 date
 echo "starting binning"
 
-export PATH="~/.conda/envs/metawrap-env/bin:$PATH"
-source ~/.bashrc
+ml anaconda
+conda activate metawrap-env
+#which python
+
+ml perl
+
+
+export PATH=/scratch4/sprehei1/AliceTurnham/lib/metaWRAP/bin/:$PATH
+#source ~/.bashrc
 
 OUTDIR=../BINNING
 
 mkdir -p $OUTDIR
 
 ASSEMBLY=../ASSEMBLY/final_assembly.fasta
-IN=../CLEAN_READS
+IN=/scratch4/sprehei1/AliceTurnham/RockCreek-Shotgun-Metagenomics-Analysis-2-ATEdits/analysis/Trimmed_FASTQ/assembled/SW_28/
 
-metawrap binning -o ${OUTDIR} -t 48 -a $ASSEMBLY --metabat2 --maxbin2 --concoct ${IN}/all_rawreads_1.fastq ${IN}/all_rawreads_2.fastq
+
+
+echo "starting metawrap"
+
+#metawrap binning -o ${OUTDIR} -t 48 -a $ASSEMBLY --metabat2 --maxbin2 --concoct ${IN}/all_1.fastq ${IN}/all_2.fastq
+
+
+#timed out, so just making concoct bins
+metawrap binning -o ${OUTDIR} -t 24 -a $ASSEMBLY --concoct ${IN}/all_1.fastq ${IN}/all_2.fastq
+
+
 
 echo "end"
 date
